6fb4885f8aefc3ec281bfab712ccd891075396a8
Esha chaturvedi <eshachaturvedi7@gmail.com>
2025-10-18 14:43:15 +0530

    Add files via upload

---
+++ Operators&itsTypes/logical.java
@@ -0,0 +1,13 @@
+public class logical {
+    public static void main(String args[]){
+       // System.out.println((3>2)&&(5<0));    // (LOGICAL AND)
+
+       // System.out.println((3>2)||(5<0));     // (LOGICAL OR)
+    
+    
+         System.out.println(!(5<0));   //(LOGICAL NOT)
+    //TRUE
+        System.out.println(!(3>2));
+        //FALSE
+    }
+}
